db190d304d6fc234491c070bf9d8d3da92a46373
Alireza Waterface <101505625+Alireza-Waterface@users.noreply.github.com>
2023-08-06 14:03:06 +0330

    Update CountryDetails.jsx

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -16,7 +16,7 @@ const CountryDetails = (props) => {
 			const response = await axios.get(`https://restcountries.com/v3.1/alpha/${cca3}`);
 			setCountry(response.data[0]);
 		} catch (error) {
-			console.error('Error fetching country? details: ', error);
+			console.error('Error fetching country details: ', error);
 		}
 	};
 
